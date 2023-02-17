1f63076efd664c81904541cf8bd0345e881c918c
MadeInUtrecht <74437330+MadeInUtrecht@users.noreply.github.com>
2023-02-17 15:44:54 +0100

    Navbar items added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,28 @@ import { BsTerminal, BsGithub } from 'react-icons/bs'
 
 const styles = {
     navbar: `w-full h-32`,
-    upperBar: `w-full h-16 bg-white flex justify-end items-center pr-8`,
+    upperBar: `w-full h-16 bg-white flex justify-start items-center pr-8`,
     lowerBar: `w-full h-16 bg-[#00ADB5]`,
-    logoDiv: `w-[15%] h-32 bg-[#00ADB5] absolute top-0 left-0 rounded-r-full flex justify-center items-center`,
+    items: `w-[100%] h-full flex justify-start items-center space-x-8 font-bold text-2xl font-mono pl-[250px]`,
+    logoDiv: `w-48 h-32 bg-[#00ADB5] absolute top-0 left-0 rounded-r-full flex justify-center items-center`,
+    icons: `absolute top-5 right-5 flex justify-center items-center `,
 }
 
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
         <div className={styles.upperBar}>
-          <BsGithub size={32} />
+          <div className={styles.icons}>
+            <BsGithub size={32} />
+          </div>
         </div>
-        <div className={styles.lowerBar}></div>
+        <div className={styles.lowerBar}>
+        <div className={styles.items}>
+            <h1>Commands</h1>
+            <h1>Projects</h1>
+            <h1>About</h1>
+          </div>
+        </div>
         <div className={styles.logoDiv}>
           <BsTerminal size={84} />
         </div>
--- src/components/TerminalTest2.jsx
+++ src/components/TerminalTest2.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 const styles = {
-  terminalBorder: `w-[100%] h-[100%] rounded-2xl flex justify-center items-center p-5`,
-  terminal: `w-[100%] h-[100%] md:w-[100%] md:h-[100%] rounded-2xl flex flex-col justify-start items-start font-mono text-white text-2xl p-11 overflow-auto bg-[#222] `,
-  input: `text-2xl text-white bg-transparent outline-none focus:outline-none`,
-  message: ``,
-};
+    terminalBorder: `w-[100%] h-[100%] rounded-2xl flex justify-center items-center p-5`,
+    terminal: `w-[100%] h-[100%] md:w-[100%] md:h-[100%] rounded-2xl flex flex-col justify-start items-start font-mono text-white text-2xl p-11 overflow-auto flex-grow bg-[#222] `,
+    input: `text-2xl text-white bg-transparent outline-none focus:outline-none  `,
+    message: ``,
+  };
+
 
 const TerminalTest2 = () => {
+
   const [input, setInput] = React.useState('');
   const [messages, setMessages] = React.useState([
     'Welcome...',
@@ -18,13 +20,15 @@ const TerminalTest2 = () => {
     'Type "exit" to close the terminal',
   ]);
   const [textColor, setTextColor] = React.useState('#ffffff');
+  const terminalRef = useRef(null);
+  const inputRef = useRef(null);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setMessages([...messages, input]);
     setInput('');
     if (input === 'help') {
-        setMessages([...messages, '-','help - show a list of commands', 'clear - clear the terminal', 'exit - close the terminal', 'color-red/green/white/yellow/blue - change text color']);
+        setMessages([...messages, '-','help - show a list of commands', 'clear - clear the terminal', 'exit - close the terminal', 'color-red/green/white/yellow/blue - change text color', 'weather-"cityname" - get the weather for a city']);
     } else if (input === 'clear') {
         setMessages(['The terminal has been cleared...']);
     } else if (input === 'exit') {
@@ -54,17 +58,36 @@ const TerminalTest2 = () => {
         setMessages([...messages, '-','Text color changed to ' + color]);
     } else if (input === 'portfolio') {
         window.open('https://www.vadimboot.com', '_blank');
-    }
-    else {
+    } else if (input.startsWith('weather-')) {
+        const city = input.slice(8);
+        const API_KEY = 'e0196a799da31cf12346606037ede7d0';
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+        try {
+          const response = await fetch(url);
+          const data = await response.json();
+          const weatherInfo = `${data.name}: ${data.weather[0].description}, temperature: ${data.main.temp} °C, feels like: ${data.main.feels_like} °C`;
+          setMessages([...messages, '-', weatherInfo]);
+        } catch (error) {
+          setMessages([...messages, '-', `Sorry, could not retrieve weather information for ${city}`]);
+        }
+    } else {
         setMessages([...messages, '-','Sorry, that is not a valid command']);
     }
 };
 
+    useEffect(() => {
+        const terminal = terminalRef.current;
+        terminal.scrollTop = terminal.scrollHeight;
+    }, [messages]);
+
+    const handleClick = () => {
+        inputRef.current.focus();
+      }
 
   return (
     <>
     <div className={styles.terminalBorder}>
-      <div className={styles.terminal}>
+      <div className={styles.terminal} ref={terminalRef} onClick={handleClick}>
         {messages.map((message, index) => (
           <p key={index} style={{color: textColor}} className={styles.message}>
             {message}
@@ -78,6 +101,7 @@ const TerminalTest2 = () => {
             type="text"
             value={input}
             onChange={(e) => setInput(e.target.value)}
+            ref={inputRef}
           ></input>
         </form>
       </div>
